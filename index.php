abe1dd2ca4abba0aac7b935d035f49f71572cc2e
Shubham Jangra <84324154+Typical-techno@users.noreply.github.com>
2023-10-13 18:15:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,250 @@
+<?php require_once('config.php'); ?>
+ <!DOCTYPE html>
+<html lang="en" class="" style="height: auto;">
+<?php require_once('inc/header.php') ?>
+<link href="https://fonts.cdnfonts.com/css/agustina" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <body>
+
+   <!-- Header
+   ================================================== -->
+   <header id="home">
+
+      <nav id="nav-wrap">
+
+         <a class="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
+        <a class="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
+
+         <ul id="nav" class="nav">
+            <li class="current"><a class="smoothscroll" href="#home">Home</a></li>
+            <li><a class="smoothscroll" href="#about">About</a></li>
+           <li><a class="smoothscroll" href="#resume">Resume</a></li>
+            <li><a class="smoothscroll" href="#portfolio">Works</a></li>
+            <li><a class="smoothscroll" href="#contact">Contact</a></li>
+         </ul> <!-- end #nav -->
+
+      </nav> <!-- end #nav-wrap -->
+<?php 
+$u_qry = $conn->query("SELECT * FROM users where id = 1");
+foreach($u_qry->fetch_array() as $k => $v){
+  if(!is_numeric($k)){
+    $user[$k] = $v;
+  }
+}
+$c_qry = $conn->query("SELECT * FROM contacts");
+while($row = $c_qry->fetch_assoc()){
+    $contact[$row['meta_field']] = $row['meta_value'];
+}
+// var_dump($contact['facebook']);
+?>
+      <div class="row banner">
+         <div class="banner-text">
+            <h1 class="responsive-headline logo" id="style_name">I'm <?php echo isset($user) ? ucwords($user['firstname'].' '.$user['lastname']) : ""; ?>.</h1>
+            <h3 style="color: #ffffff;"><?php echo stripslashes($_settings->info('welcome_message')) ?></h3>
+            <hr style="border-color: #ffffff;"/>
+            <ul class="social">
+               <li><a target="_blank" href="<?php echo $contact['instagram'] ?>"><i class="fa fa-instagram"></i></a></li>
+               <li><a target="_blank" href="<?php echo $contact['github'] ?>"><i class="fa fa-github"></i></a></li>
+               <li><a target="_blank" href="<?php echo $contact['linkin'] ?>"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+         </div>
+      </div>
+
+      <p class="scrolldown">
+         <a class="smoothscroll" href="#about"><i class="icon-down-circle"></i></a>
+      </p>
+
+   </header> <!-- Header End -->
+
+
+   <!-- About Section
+   ================================================== -->
+   <section id="about">
+
+      <div class="row">
+
+         <div class="three columns">
+
+            <img class="profile-pic"  src="<?php echo validate_image("avatar") ?>" alt="" />
+
+         </div>
+
+         <div class="nine columns main-col">
+
+            <h2>About Me</h2>
+            <style>
+              #about_me *{
+                color:#7A7A7A !important;
+              }
+            </style>
+            <div id="about_me"><?php include "about.html"; ?></div>
+
+            <div class="row">
+
+               <div class="columns contact-details">
+
+                  <h2>Contact Details</h2>
+                  <p class="address">
+               <span><?php echo $contact['address'] ?></span><br>
+               <span><?php echo $contact['mobile'] ?></span><br>
+                     <span><?php echo $contact['email'] ?></span>
+             </p>
+
+               </div>
+
+               <div class="columns download">
+                  <p>
+                     <!-- <a href="#" class="button"><i class="fa fa-download"></i>Download Resume</a> -->
+                  </p>
+               </div>
+
+            </div> <!-- end row -->
+
+         </div> <!-- end .main-col -->
+
+      </div>
+
+   </section> <!-- About Section End-->
+
+
+   <!-- Resume Section
+   ================================================== -->
+   <section id="resume">
+
+      <!-- Work
+      ----------------------------------------------- -->
+      <div class="row work">
+
+         <div class="three columns header-col">
+            <h1><span>Work</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+          <?php 
+          $w_qry = $conn->query("SELECT * FROM work ");
+          while($row = $w_qry->fetch_assoc()):
+          ?>
+            <div class="row item">
+
+               <div class="twelve columns">
+
+                  <h3><?php echo $row['company'] ?></h3>
+                  <p class="info"><?php echo $row['position'] ?> <span>&bull;</span> <em class="date"><?php echo str_replace("_"," ",$row['started']) ?> - <?php echo str_replace("_"," ",$row['ended']) ?></em></p>
+
+                  
+                  <p><?php echo stripslashes(html_entity_decode($row['description'])) ?></p>
+
+               </div>
+
+            </div> <!-- item end -->
+          <?php endwhile; ?>
+         </div> <!-- main-col end -->
+
+      </div> <!-- End Work -->
+
+
+      <!-- Skills
+      ----------------------------------------------- -->
+      <div class="row skill">
+
+         <div class="three columns header-col">
+            <h1><span>Skills</span></h1>
+         </div>
+
+         <div class="nine columns main-col">
+
+        <div class="bars">
+
+           <ul class="skills">
+            <li><span class="bar-expand photoshop"></span><em>Photoshop</em></li>
+            <li><span class="bar-expand illustrator"></span><em>Illustrator</em></li>
+            <li><span class="bar-expand wordpress"></span><em>premiere pro</em></li>
+            <li><span class="bar-expand css"></span><em>CSS</em></li>
+            <li><span class="bar-expand html5"></span><em>HTML5</em></li>
+            <li><span class="bar-expand jquery"></span><em>jQuery</em></li>
+          </ul>
+
+        </div>
+
+      </div> 
+
+      </div> 
+
+   </section>
+
+
+   <!-- Portfolio Section
+   ================================================== -->
+   <section id="portfolio">
+
+      <div class="row">
+
+         <div class="twelve columns collapsed">
+
+            <h1>Check Out Some of My Works.</h1>
+
+            <!-- portfolio-wrapper -->
+            <div id="portfolio-wrapper" class="bgrid-quarters s-bgrid-thirds cf">
+               <?php 
+                  $p_qry = $conn->query("SELECT * FROM project ");
+                  while($row = $p_qry->fetch_assoc()):
+                  ?>
+                 <div class="columns portfolio-item">
+                    <div class="item-wrap">
+
+                       <a href="#modal-<?php echo $row['id'] ?>" title="">
+                          <img alt="" src="<?php echo validate_image($row['banner']) ?>">
+                          <div class="overlay">
+                             <div class="portfolio-item-meta">
+                            <h5 class="truncate-1"><?php echo $row['name'] ?></h5>
+                                <!-- <p>Illustrration</p> -->
+                         </div>
+                          </div>
+                          <div class="link-icon"><i class="icon-plus"></i></div>
+                       </a>
+                    </div>
+                </div> <!-- item end -->
+
+              <?php endwhile; ?>
+
+            </div> <!-- portfolio-wrapper end -->
+
+         </div> <!-- twelve columns end -->
+
+
+          <?php 
+              $p_qry = $conn->query("SELECT * FROM project ");
+              while($row = $p_qry->fetch_assoc()):
+            ?>
+
+         <!-- Modal Popup
+        --------------------------------------------------------------- -->
+
+         <div id="modal-<?php echo $row['id'] ?>" class="popup-modal mfp-hide">
+
+          <img class="scale-with-grid" src="<?php echo validate_image($row['banner']) ?>" alt="" />
+
+          <div class="description-box">
+            <h4><?php echo $row['name'] ?></h4>
+            <p><?php echo stripslashes(html_entity_decode($row['description'])) ?></p>
+               <span class="categories"><i class="fa fa-tag"></i><?php echo $row['client'] ?></span>
+          </div>
+
+            <div class="link-box">
+               <!-- <a href="http://srikrishnacommunication.com/Giridesigns.html" target="_blank">Details</a> -->
+             <a class="popup-modal-dismiss">Close</a>
+            </div>
+
+        </div><!-- modal-01 End -->
+
+      <?php endwhile; ?>
+
+
+      </div> <!-- row End -->
+
+   </section> <!-- Portfolio Section End-->
+
+      <!-- /.content-wrapper -->
+      <?php require_once('inc/footer.php') ?>
+  </body>
+</html>
